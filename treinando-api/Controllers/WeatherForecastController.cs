6f06a828171383e5facf23329bdf1984a03fc877
joão victor <joaovictorf01@gmail.com>
2022-07-01 20:02:45 -0300

    Iniciando o projeto

--- treinando-api/Controllers/WeatherForecastController.cs
+++ treinando-api/Controllers/WeatherForecastController.cs
@@ -11,6 +11,7 @@ namespace treinando_api.Controllers
         "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
     };
 
+
         private readonly ILogger<WeatherForecastController> _logger;
 
         public WeatherForecastController(ILogger<WeatherForecastController> logger)
